2eebee4c2812788423a501cea0d99a84d5c074a0
Victor Hugo <161778672+VitinDuCray@users.noreply.github.com>
2025-06-01 01:42:11 -0300

    Add files via upload

---
+++ E08_SomaLinhasMatriz3x3.java
@@ -0,0 +1,28 @@
+package lista14_JAVA;
+
+import java.util.Scanner;
+
+public class E08_SomaLinhasMatriz3x3 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int[][] matriz = new int[3][3];
+
+		System.out.println("Digite os elementos da matriz 3x3:");
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 3; j++) {
+				matriz[i][j] = sc.nextInt();
+			}
+		}
+
+		for (int i = 0; i < 3; i++) {
+			int somaLinha = 0;
+			for (int j = 0; j < 3; j++) {
+				somaLinha += matriz[i][j];
+			}
+			System.out.println("Soma da linha " + (i + 1) + ": " + somaLinha);
+		}
+
+		sc.close();
+	}
+}
